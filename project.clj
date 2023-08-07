d6baddd865110a10b9da51b290447a9adcb56d4b
Sergey Trofimov <sergey@trofimov.name>
2023-08-07 14:55:04 +0200

    Set initial snapshot version to `0.1.0-SNAPSHOT`

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.strojure/PROJECT_NAME "0.1.0-01-SNAPSHOT"
+(defproject com.github.strojure/PROJECT_NAME "0.1.0-SNAPSHOT"
   :description "PROJECT_DESCRIPTION"
   :url "https://github.com/strojure/PROJECT_NAME"
   :license {:name "The Unlicense" :url "https://unlicense.org"}
